92ad0fcff400332dd6b7f74c7e1387fdec4dc27f
Nguyen Duc Tri <113581831+sh1nata-Hasu@users.noreply.github.com>
2024-08-24 22:41:27 +0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,13 +8,13 @@ import matplotlib.pyplot as plt
 
 def get_args():
     parse = argparse.ArgumentParser(description='Football Jerseys')
-    parse.add_argument('-p', '--data_path', type=str, default='../FootballDataset/football_train/')
-    parse.add_argument('-p2', '--data_path2', type=str, default='../FootballDataset/football_train/')
+    parse.add_argument('-p', '--data_path', type=str, default='./data/animals/')
+    parse.add_argument('-p2', '--data_path2', type=str, default='./data/animals/')
     parse.add_argument('-b', '--batch_size', type=int, default=16)
     parse.add_argument('-e', '--epochs', type=int, default=50)
     parse.add_argument('-l', '--lr', type=float, default=1e-2)
     parse.add_argument('-s', '--image_size', type=int, default=224)
-    parse.add_argument('-c', '--checkpoint_path', type=str, default=None) #None = train tu dau
+    parse.add_argument('-c', '--checkpoint_path', type=str, default=None) 
     parse.add_argument('-t', '--tensorboard_path', type=str, default="tensorboard")
     parse.add_argument('-r', '--trained_models', type=str, default="trained_models")
     args = parse.parse_args()
